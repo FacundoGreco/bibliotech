6d4b94c7d327bb6db2de844d7084265cea07fc57
FacundoGreco <facundogrecor@gmail.com>
2021-08-31 20:35:09 -0300

    Adds manual way to update stock

---
+++ src/model/editBooks.js
@@ -0,0 +1,15 @@
+import { db } from "./database";
+
+db.collection("books")
+	.get()
+	.then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			const title = doc.title;
+			console.log(doc.title);
+			db.collection("books")
+				.doc(doc.id)
+				.update({
+					stock: prompt(`ingrese stock ${title}`),
+				});
+		});
+	});
