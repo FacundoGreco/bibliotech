b4155d0b367c49539958eb2b1b15171455f86a90
FacundoGreco <facundogrecor@gmail.com>
2021-09-01 10:58:52 -0300

    Creates CartContext

---
+++ src/components/Contexts/CartContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+export const CartContext = createContext();
+
+export const useCartContext = () => useContext(CartContext);
+
+export function CartProvider({ children }) {
+	const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem("cartItems")));
+
+	const isInCart = (id) => {
+		return cartItems.some((item) => item.id === id);
+	};
+
+	const addItem = (item, qty) => {
+		if (!isInCart(item.id)) {
+			const cartCopy = [...cartItems];
+			cartCopy.push({ item: item, qty: qty });
+
+			setCartItems(cartCopy);
+		}
+	};
+
+	const removeItem = (id) => {
+		const cartCopy = [...cartItems];
+		cartCopy.filter((item) => item.id !== id);
+
+		setCartItems(cartCopy);
+	};
+
+	const clearCart = () => {
+		setCartItems([]);
+	};
+
+	useEffect(() => {
+		localStorage.setItem("cartItems", JSON.stringify(cartItems));
+	}, [cartItems]);
+
+	return (
+		<CartContext.Provider value={{ cartItems, isInCart, addItem, removeItem, clearCart }}>
+			{children}
+		</CartContext.Provider>
+	);
+}
