0d79e871ba45fd0da336ae25eb561b1d40620b8f
FacundoGreco <facundogrecor@gmail.com>
2021-09-07 12:54:24 -0300

    Shows itemsQty in CartWidget and unmount if empty

--- src/components/Contexts/CartContext.js
+++ src/components/Contexts/CartContext.js
@@ -5,6 +5,7 @@ export const useCartContext = () => useContext(CartContext);
 
 export function CartProvider({ children }) {
 	const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem("cartItems")) || []);
+	const [itemsQty, setItemsQty] = useState(JSON.parse(localStorage.getItem("itemsQty")) || 0);
 
 	const isInCart = (id) => {
 		return cartItems.some((item) => item.item.id === id);
@@ -16,34 +17,44 @@ export function CartProvider({ children }) {
 			cartCopy.push({ item: item, qty: qty });
 
 			setCartItems(cartCopy);
+			setItemsQty(itemsQty + qty);
 		}
 	};
 
-	const updateItemQty = (id, qty) => {
+	const updateItemQty = (id, newQty) => {
 		const cartCopy = [...cartItems];
-		cartCopy.find((item) => item.item.id === id).qty = qty;
+		const item = cartCopy.find((item) => item.item.id === id);
+		const qty = item.qty;
+
+		item.qty = newQty;
 
 		setCartItems(cartCopy);
+		setItemsQty(itemsQty - qty + newQty);
 	};
 
 	const removeItem = (id) => {
 		let cartCopy = [...cartItems];
+		const qty = cartCopy.find((item) => item.item.id === id).qty;
 		cartCopy = cartCopy.filter((item) => item.item.id !== id);
 
 		setCartItems(cartCopy);
+		setItemsQty(itemsQty - qty);
 	};
 
 	const clearCart = () => {
 		setCartItems([]);
+		setItemsQty(0);
 	};
 
 	useEffect(() => {
 		//SAVES CART ITEMS IN LOCAL STORAGE
 		localStorage.setItem("cartItems", JSON.stringify(cartItems));
-	}, [cartItems]);
+		//SAVES ITEMS QUANTITY IN LOCAL STORAGE
+		localStorage.setItem("itemsQty", JSON.stringify(itemsQty));
+	}, [cartItems, itemsQty]);
 
 	return (
-		<CartContext.Provider value={{ cartItems, isInCart, addItem, updateItemQty, removeItem, clearCart }}>
+		<CartContext.Provider value={{ cartItems, itemsQty, isInCart, addItem, updateItemQty, removeItem, clearCart }}>
 			{children}
 		</CartContext.Provider>
 	);
--- src/components/Header/CartWidget.js
+++ src/components/Header/CartWidget.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useCartContext } from "../Contexts/CartContext";
 import "./CartWidget.scss";
 
 export default function CartWidget() {
+	const { itemsQty } = useCartContext();
+
 	return (
-		<img
-			className={"cartButton"}
-			src={"https://i.ibb.co/vmrYNNp/cart.png"}
-			alt={"Carrito"}
-		></img>
+		<>
+			{itemsQty > 0 && (
+				<div className="cartWidget">
+					<img className="cartButton" src={"https://i.ibb.co/vmrYNNp/cart.png"} alt={"Carrito"}></img>
+					<p>{itemsQty}</p>
+				</div>
+			)}
+		</>
 	);
 }
