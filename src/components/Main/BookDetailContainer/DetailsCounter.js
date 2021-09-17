25d862ed48181f22edd3e60d1715dd2d4331719b
FacundoGreco <facundogrecor@gmail.com>
2021-09-17 11:24:25 -0300

    Fixes outOfStock item functionality in BookDetails

--- src/components/Main/BookDetailContainer/DetailsCounter.js
+++ src/components/Main/BookDetailContainer/DetailsCounter.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Counter from "../Counter/Counter";
 
 export default function DetailsCounter({ max, handleAddItem }) {
-	const [qty, setQty] = useState(1);
+	const [qty, setQty] = useState(max !== 0 ? 1 : 0);
 
 	return (
 		<div className="addToCart">
-			<Counter max={max} qty={qty} setQty={setQty} />
+			<Counter max={max} qty={qty <= max ? qty : max} setQty={setQty} />
 
 			<button
+				disabled={max === 0 ? true : false}
 				onClick={() => {
 					handleAddItem(qty);
 				}}
